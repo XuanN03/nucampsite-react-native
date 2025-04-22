016fc96fc6b46abc7db72d2a41d98f6c81a13814
Xuan N <xtnguyen03@gmail.com>
2025-04-22 16:01:42 -0400

    tab navigations

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -19,6 +19,8 @@ import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from "./ReservationScreen";
 import FavoritesScreen from "./FavoritesScreen";
 import LoginScreen from "./LoginScreen";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/core";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 const Drawer = createDrawerNavigator();
 
@@ -154,10 +156,13 @@ const LoginNavigator = () => {
             <Stack.Screen 
                 name="Login"
                 component={LoginScreen}
-                options={({navigation})=> ({
+                options={({navigation, route})=> ({
+                    headerTitle: getFocusedRouteNameFromRoute(route),
                     headerLeft:()=> (
                         <Icon 
-                            name='sign-in'
+                            name={
+                                getFocusedRouteNameFromRoute(route) === 'Register' ? 'user-plus' : 'sign-in'
+                            }
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
